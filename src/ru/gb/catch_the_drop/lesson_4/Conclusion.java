e6f37085d9bdcc364246ab5e38a0d30735829d6c
Evgeny-mea <70842210+Evgeny-mea@users.noreply.github.com>
2020-11-01 21:20:11 +0700

    lesson_0022222_28102020

---
+++ src/ru/gb/catch_the_drop/lesson_4/Conclusion.java
@@ -0,0 +1,25 @@
+package ru.gb.catch_the_drop.lesson_4;
+
+public class Conclusion {
+
+    public static void main (String[]args) {
+        Employees a = new Employees("Иван", "Программист", 231 - 98, 30000, 32);
+        Employees b = new Employees("Коля", "Менеджер", 231 - 98, 30000, 39);
+        Employees c = new Employees("Ольга", "Зам.Директора", 231 - 98, 30000, 42);
+        Employees d = new Employees("Мария", "Директор", 231 - 98, 30000, 34);
+        Employees f = new Employees("Александра", "Бухгалтер", 231 - 98, 30000, 54);
+
+        Employees[] nums = new Employees[5];
+        nums[0] = a;
+        nums[1] = b;
+        nums[2] = c;
+        nums[3] = d;
+        nums[4] = f;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i].getAge() >= 40)
+                System.out.printf("    ФИО: %s,             Должность: %s,      Зарплата: %d,       Телефон: %d,        Возраст: %d \n",
+                        nums[i].getName(), nums[i].getPosition(), nums[i].getIncome(), nums[i].getPhone(), nums[i].getAge());
+        }
+    }
+}
---
+++ src/ru/gb/catch_the_drop/lesson_4/Employees.java
@@ -0,0 +1,33 @@
+package ru.gb.catch_the_drop.lesson_4;
+
+public class Employees {
+    private String name;        // ФИО
+    private String position ;   // Должность
+    private int tel;            // Телефон
+    private int income;         // Зарплата
+    private int age;            // Возраст
+
+    Employees(String name, String zp, int tel, int income, int age) {
+        this.name = name;
+        this.position = zp;
+        this.tel = tel;
+        this.income = income;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getPosition() {
+        return position;
+    }
+    public int getPhone(){
+        return tel;
+    }
+    public int getIncome(){
+        return income;
+    }
+    public int getAge() {
+        return age;
+    }
+ }
