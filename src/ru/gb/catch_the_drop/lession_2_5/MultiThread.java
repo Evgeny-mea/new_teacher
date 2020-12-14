66df13adacbf911c63056475a3353fbb5b0f3f29
Evgeny-mea <70842210+Evgeny-mea@users.noreply.github.com>
2020-12-14 22:32:48 +0700

    14.12.20

--- src/ru/gb/catch_the_drop/lession_2_5/MultiThread.java
+++ src/ru/gb/catch_the_drop/lession_2_5/MultiThread.java
@@ -38,7 +38,8 @@ public class MultiThread {
     private static void methodOne (float[] arr) {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (float) (arr[i] *
-                    Math.sin(0.2f + i / 5) *
+                    Math.sin(0.2f + i / 5)
+                    *
                     Math.cos(0.2f + i / 5) *
                     Math.cos(0.4f + i / 2));
         }
